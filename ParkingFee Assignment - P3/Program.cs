a91814a9ba9414fd566507195a9803c262266677
R0MaD <adrian.romeo@outlook.com>
2018-11-28 22:20:58 +1030

    updated repo

--- ParkingFee Assignment - P3/Program.cs
+++ ParkingFee Assignment - P3/Program.cs
@@ -23,7 +23,7 @@ namespace ParkingFee_Assignment___P3
             
             //declare variables
             decimal parkingFee;
-            int hours = 0;
+            decimal totalFee = 0;
             decimal averageParkingFee = 0;
             
             //copyright
@@ -31,37 +31,36 @@ namespace ParkingFee_Assignment___P3
             Console.WriteLine(" ParkingFee3 program developed by: Adrian Romeo");
             Console.WriteLine("-----------------------------------------------\n");
             
-            //input - data read from hours.txt file and stored into array hours 
+            /*input - read data from "hours.txt" file and stored into memory positions by creating an array to store the 50 entries
+            (assigned a constant to make the program adaptable for easy upgradeability) and using StreamReader class to read line by line 
+            and assign their value to an array position until line value is null, then close the file to avoid errors
+             */ 
             int[] hoursArray = new int[MAX_ARRAY_SIZE]; //array
             StreamReader fileSR = new StreamReader("hours.txt"); //file the entries are readed from
-            int counter = 0;
-            string line = "";
+            int counter = 0; //array counter
+            string line = ""; //declare an empty line
             line = fileSR.ReadLine(); //start reading file
             while (line != null) //while not end of file
             {
-                hoursArray[counter] = int.Parse(line);
+                hoursArray[counter] = int.Parse(line); //convert string into int and assign it to array position
                 counter = counter + 1;
-                line = fileSR.ReadLine();
+                line = fileSR.ReadLine(); //continue reading next line
             }
             fileSR.Close(); //finished reading file
 
-            StreamWriter fileSW = new StreamWriter("hours.txt"); 
+            StreamWriter fileSW = new StreamWriter("hours.txt");
             for (int i = 0; i < counter; i++)
-                {
+            {
                 parkingFee = hoursArray[i] * HOURLY_RATE;
-                 if (parkingFee >= MAX_FEE) //calculates parking fee by multiplying hours by the hourly rate, then compares to MAX_FEE as the maximum value
-                    {
-                    parkingFee = MAX_FEE;
-                    }
-                fileSW.WriteLine("{0},{1}", hoursArray[i], parkingFee.ToString("N2"));  //writes into text file
-                }
-            fileSW.Close(); //finished writting to file
-            for (int index = 0; index < counter; index++)
+                if (parkingFee >= MAX_FEE) //calculates parking fee by multiplying hours by the hourly rate, then compares to MAX_FEE as the maximum value
                 {
-                    hours = hours + hoursArray[index];
+                    parkingFee = MAX_FEE;
                 }
-            averageParkingFee = (decimal) hours / hoursArray.Length;
-
+                totalFee = totalFee + parkingFee; //adding all parkingFee values
+                averageParkingFee = totalFee / MAX_ARRAY_SIZE; //formula to calculate average
+                fileSW.WriteLine("{0},{1}", hoursArray[i], parkingFee.ToString("N2"));  //writes into text file
+            }
+            fileSW.Close(); //finished writting to file
             //screen output
             Console.WriteLine();
             Console.WriteLine("Average parking fee: {0}", averageParkingFee.ToString("N2"));
